2edee3460117c7e313a74af799c66d6392c74c7e
Gergely Tankovics <gergely.tankovics@gmail.com>
2019-10-15 18:33:40 +0200

    handling invalid clusterName in argument

--- chenv.sh
+++ chenv.sh
@@ -22,12 +22,17 @@ else
             SELECTED_CONFIG=$1
         fi
         if [ $1 == $GCP_CURRENT_CONFIG ]; then
-            echo -n "$1 is the current config. "
+            echo "$1 is the current config. "
             if [ ! -z $2 ]; then
                 if [ $2 == $K8S_CLUSTER_SHORT ]; then
                     echo "$2 is the current cluster."
                 else
                     SET_PROJECT=true
+                    for CLUSTER in $(gcloud container clusters list --format='value(name)'); do
+                        if [ $CLUSTER == $2 ]; then
+                            VALID_CLUSTER=true
+                        fi
+                    done
                 fi
             fi
             VALID_PROJECT=true
@@ -78,8 +83,14 @@ else
                             echo "Cluster not specified in configurations. Use the first cluster. ${CLUSTERS[0]}"
                             CLUSTER=${CLUSTERS[0]}
                         else
-                            echo "Use $2 for 'kubectl'"
-                            CLUSTER=$2
+                            if [ $VALID_CLUSTER ]; then
+                                echo "Use $2 for 'kubectl'"
+                                CLUSTER=$2
+                            else 
+                                echo "$GOOGLE_PROJECT does not contain $2 cluster. 'kubectl' context does not change."
+                                echo "Valid clusters ${CLUSTERS}"
+                                exit
+                            fi
                         fi
                     else 
                         echo "Use $CLUSTER from configuration."
